13981355770c31abf4db1e4e9df0e14f7207a9de
Bernhard Waldbrunner <vbwx01@gmail.com>
2024-01-27 16:28:29 +0100

    Update lint.mk

--- include/lint.mk
+++ include/lint.mk
@@ -1,12 +1,10 @@
 .PHONY: perllint perlcritic lint
 
-PERLCRITIC = $(INSTALLSITEBIN)/perlcritic
-
 perllint:
 	-$(NOECHO) $(PERLRUN) -MO=Lint -cw finddup 2>&1 | grep -v "syntax OK" | grep -v "Can't locate"
 
 perlcritic:
-	-$(NOECHO) $(PERLCRITIC) .
+	-$(NOECHO) $(INSTALLSITEBIN)/perlcritic .
 
 lint: perllint perlcritic
 
