02810e5857d6c0b694bdbf6c57f965b17594fa06
Bernhard Waldbrunner <vbwx01@gmail.com>
2025-01-28 18:40:05 +0100

    Update manual.mk

--- include/manual.mk
+++ include/manual.mk
@@ -1,3 +1,3 @@
 Manual.md: finddup
 	$(NOECHO) $(PERLRUN) -MPod::Markdown -e 'Pod::Markdown->new->filter(@ARGV)' $< | \
-		$(PERLRUN) -p0e 's/<!--.*?-->\n//gs; s/--/-\\-/g' > $@
+		$(PERLRUN) -p0e 's/<!--.*?-->\n?//gs; s/--/-\\-/g' > $@
