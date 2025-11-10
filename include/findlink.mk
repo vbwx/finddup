4f9038762263bf1cc08e774475c6161105960b48
Bernhard Waldbrunner <vbwx01@gmail.com>
2025-11-10 22:08:18 +0100

    Force creation of symlinks

--- include/findlink.mk
+++ include/findlink.mk
@@ -1,8 +1,8 @@
 pure_site_install ::
-	$(NOECHO) ln -s finddup $(DESTINSTALLSITEBIN)/findlink
+	$(NOECHO) ln -sf finddup $(DESTINSTALLSITEBIN)/findlink
 
 doc_site_install ::
-	$(NOECHO) ln -s finddup.1 $(DESTINSTALLSITEMAN1DIR)/findlink.1
+	$(NOECHO) ln -sf finddup.1 $(DESTINSTALLSITEMAN1DIR)/findlink.1
 
 uninstall_from_sitedirs ::
 	$(NOECHO) rm -f $(DESTINSTALLSITEBIN)/findlink $(DESTINSTALLSITEMAN1DIR)/findlink.1
